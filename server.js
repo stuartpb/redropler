836a4dbcf0e3aa5dbecbddc3f0c64e1d95be155f
Stuart P. Bentley <stuart@testtrack4.com>
2014-08-28 21:52:04 +0000

    Add optional command to execute before restoring the droplet

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var spawn = require('child_process').spawn;
 var request = require('request');
 
+var dropletId = process.env.DROPLET_ID;
+var dropletDomain = process.env.DROPLET_DOMAIN;
+var sshIdentity = process.env.SSH_IDENTITY;
+var sshUsername = process.env.SSH_USERNAME;
+var preResetCommand = process.env.PRE_RESET_COMMAND;
+var preResetCommandTimeout = process.env.PRE_RESET_COMMAND_TIMEOUT;
+
+var defaultPreResetCommand =
+  'echo "The system is going down for reset NOW!" | wall';
+
+var sshCommand = [
+  'temp=`mktemp`',
+  'printf "%s" "$0" >> "$temp"',
+  'timeout $4 ' +
+    'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' +
+      '-i "$temp" "$1@$2" -- "$3"',
+  'rm "$temp"'].join('\n');
+
 var api_endpoint = "https://api.digitalocean.com/v2/droplets/" +
-  process.env.DROPLET_ID + "/actions";
+  dropletId + "/actions";
 
 var app = express();
 
-app.set('dropletDomain',process.env.DROPLET_DOMAIN);
+app.set('dropletDomain',dropletDomain);
 
 app.use(express.static(__dirname + '/static'));
 
@@ -18,7 +37,26 @@ app.get('/', function(req, res) {
   res.render('index.jade');
 });
 
-app.post('/', function(req, res, next) {
+function sendCommand(req, res, next) {
+  var sshProc = spawn('bash', ['-c', sshCommand,
+    sshIdentity+'\n',
+    sshUsername || 'root',
+    dropletDomain,
+    preResetCommand || defaultPreResetCommand,
+    preResetCommandTimeout || 5]);
+
+  sshProc.stdout.on('data', function (data) {
+    console.log('ssh stdout: ' + data);
+  });
+  sshProc.stderr.on('data', function (data) {
+    console.log('ssh stderr: ' + data);
+  });
+  sshProc.on('close', function (code) {
+    next();
+  });
+}
+
+function sendReset(req, res, next) {
   request.post(api_endpoint,{
     body: {type: 'restore', image: process.env.RESTORE_IMAGE_ID},
     auth: {bearer: process.env.DIGITALOCEAN_API_TOKEN}, json: true},
@@ -28,7 +66,11 @@ app.post('/', function(req, res, next) {
         return next(body.message ? new Error(body.message) : body);
       res.redirect('/status/' + body.action.id);
     });
-});
+}
+
+var postStack = [sendReset];
+if (sshIdentity && dropletDomain) postStack.unshift(sendCommand);
+app.post('/', postStack);
 
 app.get('/status/:action', function(req, res, next) {
   request.get(api_endpoint + '/' + req.params.action, {
