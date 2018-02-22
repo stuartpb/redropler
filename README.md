73be2fb1da8cca22c7d6881de32787d6758f0920
Stuart P. Bentley <stuart@testtrack4.com>
2018-02-22 00:01:43 +0000

    Add Packet reinstallation support

--- README.md
+++ README.md
@@ -8,21 +8,30 @@ reshotput is a [12-Factor App](http://12factor.net), with all configuration perf
 
 ## API parameters
 
-Right now, DigitalOcean is the only supported server host: support for more hosts is planned via the [pkgcloud](https://github.com/pkgcloud/pkgcloud) library.
+Right now, DigitalOcean and Packet are the only supported server hosts: support for more hosts is planned via the [pkgcloud](https://github.com/pkgcloud/pkgcloud) library.
+
+### General
+
+- `SNAPSHOT_ID`: The ID of the image to restore.
+- `SERVER_ID`: The ID of the server to restore the image to.
+- `SERVER_ADDRESS`: The location of the server, for use when referring to the server in messages, and when connecting to run the SSH pre-reset command (see below).
+
+How the `SNAPSHOT_ID` and `SERVER_ID` parameters will be interpreted depends on which of the following parameters are present:
 
 ### DigitalOcean
 
 - `DIGITALOCEAN_API_TOKEN`: The API token to use for authentication.
-- `RESTORE_IMAGE_ID`: The ID of the image to restore.
-- `DROPLET_ID`: The ID of the droplet to restore the image to.
 
-`RESTORE_IMAGE_ID` and `DROPLET_ID` will probably be general-purpose renamed to `SNAPSHOT_ID` and `SERVER_ID` in the future.
+### Packet
+
+- `PACKET_API_KEY`: The API key to use for authentication.
+
+Note that, as Packet does not support any kind of snapshotting for a system's on-board storage, Packet will only trigger a reinstall of the server, ignoring any value of `SNAPSHOT_ID`.
 
 ## SSH Pre-reset Commands
 
 reshotput includes functionality to log in and perform a command on the server via SSH before resetting it.
 
-- `DROPLET_DOMAIN`: The location of the server to connect to. *(This will be renamed soon, probably to something like `SERVER_ADDRESS`.)*
 - `SSH_IDENTITY`: The content of the private key to use when logging into the server.
 - `SSH_USERNAME`: The name of the user to log in as. Defaults to `root`.
 - `PRE_RESET_COMMAND`: The command to execute before performing the reset. Defaults to `echo "The system is going down for reset NOW!" | wall`.
--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var spawn = require('child_process').spawn;
-var request = require('request');
 
-var dropletId = process.env.DROPLET_ID;
-var dropletDomain = process.env.DROPLET_DOMAIN;
+var serverId = process.env.SERVER_ID;
+var snapshotId = process.env.SNAPSHOT_ID;
+var serverAddress = process.env.SERVER_ADDRESS;
 var sshIdentity = process.env.SSH_IDENTITY;
 var sshUsername = process.env.SSH_USERNAME;
 var preResetCommand = process.env.PRE_RESET_COMMAND;
 var preResetCommandTimeout = process.env.PRE_RESET_COMMAND_TIMEOUT;
+
+var service;
+
+if (process.env.DIGITALOCEAN_API_TOKEN) {
+  service = require('./lib/digitalOcean.js')({
+    apiToken: process.env.DIGITALOCEAN_API_TOKEN,
+    dropletId: serverId,
+    snapshotId: snapshotId
+  });
+} else if (process.env.PACKET_API_KEY) {
+  service = require('./lib/packet.js')({
+    apiKey: process.env.PACKET_API_KEY,
+    deviceId: serverId,
+  });
+} else {
+  console.error('No compatible service token/key defined');
+  process.exit(1);
+}
 
 var defaultPreResetCommand =
   'echo "The system is going down for reset NOW!" | wall';
@@ -20,12 +38,9 @@ var sshCommand = [
     '-i "$temp" "$2@$3" -- "$4"',
   'rm "$temp"'].join('\n');
 
-var api_endpoint = "https://api.digitalocean.com/v2/droplets/" +
-  dropletId + "/actions";
-
 var app = express();
 
-app.set('dropletDomain',dropletDomain);
+app.set('serverAddress', serverAddress);
 
 app.use(express.static(__dirname + '/static'));
 
@@ -40,7 +55,7 @@ function sendCommand(req, res, next) {
   var sshProc = spawn('bash', ['-c', sshCommand, 'sshCommand',
     sshIdentity+'\n',
     sshUsername || 'root',
-    dropletDomain,
+    serverAddress,
     preResetCommand || defaultPreResetCommand,
     preResetCommandTimeout || 5]);
 
@@ -70,31 +85,10 @@ function sendCommand(req, res, next) {
     preResetCommandTimeout ? preResetCommandTimeout * 1000 : 5000);
 }
 
-function sendReset(req, res, next) {
-  request.post(api_endpoint,{
-    body: {type: 'restore', image: process.env.RESTORE_IMAGE_ID},
-    auth: {bearer: process.env.DIGITALOCEAN_API_TOKEN}, json: true},
-    function(err, response, body) {
-      if (err) return next(err);
-      if (!/^2/.test(response.statusCode))
-        return next(body.message ? new Error(body.message) : body);
-      res.redirect('/status/' + body.action.id);
-    });
-}
-
-var postStack = [sendReset];
-if (sshIdentity && dropletDomain) postStack.unshift(sendCommand);
+var postStack = [service.sendReset];
+if (sshIdentity && serverAddress) postStack.unshift(sendCommand);
 app.post('/', postStack);
 
-app.get('/status/:action', function(req, res, next) {
-  request.get(api_endpoint + '/' + req.params.action, {
-    auth: {bearer: process.env.DIGITALOCEAN_API_TOKEN}, json: true},
-    function(err, response, body) {
-      if (err) return next(err);
-      if (!/^2/.test(response.statusCode))
-        return next(body.message ? new Error(body.message) : body);
-      res.render('status.jade', body);
-    });
-});
+app.get('/status/:action', service.getStatus);
 
 app.listen(process.env.PORT || 5000, process.env.IP);
